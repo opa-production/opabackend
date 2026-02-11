c98f294f49c2597b905e78fc74db8fbf153d8871
Ericode <jilloerick6@gmail.com>
2026-02-11 09:37:06 +0300

    fix: update passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,17 +1,9 @@
-from fastapi import FastAPI
-from mangum import Mangum  # Optional for ASGI-to-WSGI wrapper
 import sys
 import os
 
-# Include your app directory in the path
 sys.path.insert(0, os.path.dirname(__file__))
 
-from app.main import app  # your FastAPI app
+from app.main import app
 
-# If your hosting requires WSGI:
-try:
-    from mangum import Mangum
-    application = Mangum(app)
-except ImportError:
-    # fallback: use as ASGI app directly
-    application = app
+application = app
+
